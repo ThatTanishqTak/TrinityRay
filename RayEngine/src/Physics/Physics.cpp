a3d1785253179ea87180310e101a620e65d93361
ThatTanishqTak <tanishqtak3@gmail.com>
2025-04-16 18:33:04 +0100

    2D gravity is working

--- RayEngine/src/Physics/Physics.cpp
+++ RayEngine/src/Physics/Physics.cpp
@@ -5,20 +5,40 @@ namespace Engine
 {
 	void Physics::Update()
 	{
-		const float gravity = 9.81f;
-
-		m_Velocity.y += gravity * GetFrameTime();
-		m_RectPosition.y += m_Velocity.y * GetFrameTime();
-
-		// Collision with ground
-		if (m_RectPosition.y >= GetScreenHeight())
+		for (Rectangle& rect : m_Rectangles)
 		{
-			m_RectPosition.y = 0.0f;
+			rect.y += m_Gravity * m_Multiplier * GetFrameTime();
+
+			std::cout << "Rectangle " << m_count << ": " << rect.x << " " << rect.y << std::endl;
+
+			// Collision with ground
+			if (rect.y >= GetScreenHeight())
+			{
+				rect.y = 0.0f;
+			}
 		}
+
+		AddRectangle();
 	}
 
 	void Physics::Render()
 	{
-		DrawRectanglePro({ m_RectPosition.x, m_RectPosition.y, 100.0f, 100.0f }, { 0.0f, 100.0f }, 0.0f, RED);
+		for (Rectangle& rect : m_Rectangles)
+		{
+			DrawRectanglePro(rect, { 0.0f, 0.0f }, 0.0f, RED);
+		}
+	}
+
+	void Physics::AddRectangle()
+	{
+		if (IsKeyPressed(KEY_R))
+		{
+			m_count++;
+			
+			int l_RandomPosX = rand() % GetScreenWidth();
+			Rectangle l_Rect = { (float)l_RandomPosX, 0.0f, 50.0f, 50.0f };
+
+			m_Rectangles.push_back(l_Rect);
+		}
 	}
 }
--- RayEngine/src/Physics/Physics.h
+++ RayEngine/src/Physics/Physics.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <raylib.h>
+#include <list>
 
 namespace Engine
 {
@@ -11,7 +12,17 @@ namespace Engine
 		void Render();
 
 	private:
+		const float m_Gravity = 9.81f;
+		const float m_Multiplier = 5.0f;
+
+		int m_count = 0;
+
 		Vector2 m_RectPosition = { 0.0f, 0.0f };
 		Vector2 m_Velocity = { 0.0f, 0.0f };
+
+		std::list<Rectangle> m_Rectangles;
+
+	private:
+		void AddRectangle();
 	};
 }
